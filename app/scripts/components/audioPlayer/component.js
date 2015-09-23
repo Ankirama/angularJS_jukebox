36540027d82b710c83ec79e2544aeb38a622b738
Ankirama <r4z0r91@gmail.com>
2015-09-23 02:27:00 +0200

    auto play after music ended

--- app/scripts/components/audioPlayer/component.js
+++ app/scripts/components/audioPlayer/component.js
@@ -7,10 +7,6 @@ angular.module('audioPlayer-directive', [])
                 $scope.audio = new Audio();
                 $scope.currentNum = 0;
 
-                // tell others to give me my prev/next track (with audio.set message)
-                $scope.next = function(){ $rootScope.$broadcast('audio.next'); };
-                $scope.prev = function(){ $rootScope.$broadcast('audio.prev'); };
-
                 // change volume and track position
                 $scope.volume = 1;
 
@@ -21,11 +17,16 @@ angular.module('audioPlayer-directive', [])
                 // tell audio element to play/pause, you can also use $scope.audio.play() or $scope.audio.pause();
                 $scope.playpause = function(){ var a = $scope.audio.paused ? $scope.audio.play() : $scope.audio.pause(); };
 
+                // tell others to give me my prev/next track (with audio.set message)
+                $scope.next = function(){ $rootScope.$broadcast('audio.next'); $scope.audio.paused? $scope.audio.pause() : $scope.audio.play(); };
+                $scope.prev = function(){ $rootScope.$broadcast('audio.prev'); $scope.audio.paused? $scope.audio.pause() : $scope.audio.play(); };
+
+
                 // listen for audio-element events, and broadcast stuff
                 $scope.audio.addEventListener('play', function(){ $rootScope.$broadcast('audio.play', this); });
                 $scope.audio.addEventListener('pause', function(){ $rootScope.$broadcast('audio.pause', this); });
                 $scope.audio.addEventListener('timeupdate', function(){ $rootScope.$broadcast('audio.time', this); });
-                $scope.audio.addEventListener('ended', function(){ $rootScope.$broadcast('audio.ended', this); $scope.next(); });
+                $scope.audio.addEventListener('ended', function(){ $rootScope.$broadcast('audio.ended', this); $scope.next(); $scope.audio.play(); });
 
                 // respond to set track messsages
                 $rootScope.$on('audio.set', function(r, file, info, currentNum, totalNum){
